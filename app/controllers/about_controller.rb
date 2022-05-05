488732af99af2b2cb1122d4f8896182ec4df1c2d
warlocksa <warlock_sa@hotmail.com>
2022-05-04 22:19:22 -0600

    create the simple erb for about controller, and add the link at nav-top and bot

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,10 +1,4 @@
 class AboutController < ApplicationController
   def index
-    @products = Product.all.order(created_at: :desc)
   end
-
-  def show
-    @product = Product.find params[:id]
-  end
-
 end
