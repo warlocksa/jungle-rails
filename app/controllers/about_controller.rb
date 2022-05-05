4f0db610b711c19037ebd9bcab14aa0b0ad109d5
warlocksa <warlock_sa@hotmail.com>
2022-05-04 21:08:30 -0600

    change the link website into path in admin_show part

---
+++ app/controllers/about_controller.rb
@@ -0,0 +1,10 @@
+class AboutController < ApplicationController
+  def index
+    @products = Product.all.order(created_at: :desc)
+  end
+
+  def show
+    @product = Product.find params[:id]
+  end
+
+end
