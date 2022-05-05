34c108f98526a07d71fb579c59e190fb7b5ef707
warlocksa <warlock_sa@hotmail.com>
2022-05-05 17:53:09 -0600

    delete the test part of /cool and /sweet

--- config/routes.rb
+++ config/routes.rb
@@ -22,13 +22,9 @@ Rails.application.routes.draw do
     resources :categories, only: [:index, :new, :create]
   end
   
-  get '/cool' => 'gif#cool'
-  get '/sweet' => 'gif#sweet'
-
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
-
     # These routes will be for signup. The first renders a form in the browse, the second will 
     # receive the form and create a user in our database using the data given to us by the user.
   get '/signup' => 'users#new'
