faa47892bf308d0182a38a5b2777b0db6b82b8f3
warlocksa <warlock_sa@hotmail.com>
2022-05-09 16:24:32 -0600

    add the test file to test the product and pass the test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_05_225841) do
+ActiveRecord::Schema.define(version: 2022_05_09_213347) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,11 @@ ActiveRecord::Schema.define(version: 2022_05_05_225841) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "stripe_charge_id"
     t.string "email"
+  end
+
+  create_table "product_specs", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "products", force: :cascade do |t|
