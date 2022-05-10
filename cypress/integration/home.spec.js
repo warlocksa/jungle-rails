dcd9f54e4f0b658b9c81258b9013667d41feb0ae
warlocksa <warlock_sa@hotmail.com>
2022-05-10 16:25:57 -0600

    add the cypress test files about home page

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,13 @@
+
+describe('jungle-rails', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/')
+  })
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+  it("There is 12 products on the page", () => {
+    cy.get(".products article").should("have.length", 12);
+  }); 
+})
